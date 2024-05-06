9f7cc6f71b3af393ea39d95fe5b708e664c08040
Serhii Buryk <sergmarten21@gmail.com>
2024-05-06 05:17:09 +0300

    settings: add default feeds value to overwrite exist json file

--- menu/menu/settings.py
+++ menu/menu/settings.py
@@ -100,6 +100,13 @@ REQUEST_FINGERPRINTER_IMPLEMENTATION = "2.7"
 TWISTED_REACTOR = "twisted.internet.asyncioreactor.AsyncioSelectorReactor"
 FEED_EXPORT_ENCODING = "utf-8"
 
+FEEDS = {
+    'data.json': {
+        'format': 'json',
+        'overwrite': True,
+    },
+}
+
 # Logging settings
 
 # LOG_LEVEL = "DEBUG"
